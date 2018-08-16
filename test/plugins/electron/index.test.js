68f2775410042d3a808c8ea393381b01cbc0f098
William Wall <wwall@caci.com>
2018-08-16 16:00:35 -0600

    feat(electron): Add resolver for packaged electron deps
    Most dependencies for this build are packaged into the web app itself. However, Electron apps allow
    
    the use of the web app alongside typical node.js packages. Since those dependencies must be packaged
    
    by the electron-package project, we will generate an app/package.json for it to read from all the
    
    available plugins.

---
+++ test/plugins/electron/index.test.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+const path = require('path');
+const expect = require('chai').expect;
+const electron = require('../../../plugins/electron');
+const rimraf = require('rimraf');
+const utils = require('../../../utils.js');
+
+describe('electron resolver', () => {
+  afterEach(() => {
+    electron.clear();
+    rimraf.sync(path.join(outputDir, '*'));
+  });
+
+  var outputDir = path.join(process.cwd(), '.test');
+  var baseDir = path.join(__dirname, 'electron');
+  var dirs = fs.readdirSync(baseDir);
+
+  dirs.forEach((d) => {
+    var dir = path.join(baseDir, d, 'project');
+
+    try {
+      var pack = require(dir + '/package');
+    } catch (e) {
+      console.error(e);
+    }
+
+    if (pack) {
+      it(d.replace(/-/g, ' '), () => {
+        return electron.resolver(pack, dir, 0)
+          .then(() => {
+            // mock this to just find the sibling directories
+            var old = utils.resolveModulePath;
+            utils.resolveModulePath = (p) => {
+              return path.join(baseDir, d, p);
+            };
+
+            var promise = electron.writer(pack, outputDir);
+
+            // put it back
+            utils.resolveModulePath = old;
+            return promise;
+          })
+          .finally(() => {
+            try {
+              var expected = require(path.join(baseDir, d, 'expected'));
+            } catch (e) {
+              expected = false;
+            }
+
+            try {
+              var generatedAppPack = require(path.join(baseDir, d, 'opensphere-electron', 'app', 'package'));
+            } catch (e) {
+              generatedAppPack = false;
+            }
+
+            expect(generatedAppPack).to.deep.equal(expected);
+          });
+      });
+    }
+  });
+});
